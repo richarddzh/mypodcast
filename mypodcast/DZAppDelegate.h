c89e5eaa09577a2e31f52cd4c4a9dbea7dfa4a38
Richard Dong <richarddzh@gmail.com>
2014-07-31 14:36:02 +0800

    Initial Commit

---
+++ mypodcast/DZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DZAppDelegate.h
+//  mypodcast
+//
+//  Created by Richard Dong on 14-7-31.
+//  Copyright (c) 2014年 Richard Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mypodcast/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mypodcast
+//
+//  Created by Richard Dong on 14-7-31.
+//  Copyright (c) 2014年 Richard Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DZAppDelegate class]));
+    }
+}
