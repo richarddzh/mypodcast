f4af50c6c5281c826d8f637abafe468aa578523f
Richard Dong <richarddzh@gmail.com>
2014-09-20 20:48:08 +0800

    Rewrite transparent image generation. use https://gist.github.com/benilovj/2009030. To avoid unsupported parameter combination error when create bitmap graphic context.

--- mypodcast/gui/UIImage+DZImagePool.m
+++ mypodcast/gui/UIImage+DZImagePool.m
@@ -10,6 +10,58 @@
 
 static NSMutableDictionary * _imagePool;
 
+@interface UIImage (DZImagePrivate)
+- (BOOL)hasAlpha;
+- (UIImage *)imageWithAlpha:(CGFloat)alpha;
+@end
+
+@implementation UIImage (DZImagePrivate)
+
+// Returns true if the image has an alpha layer
+- (BOOL)hasAlpha
+{
+    CGImageAlphaInfo alpha = CGImageGetAlphaInfo(self.CGImage);
+    return (alpha == kCGImageAlphaFirst ||
+            alpha == kCGImageAlphaLast ||
+            alpha == kCGImageAlphaPremultipliedFirst ||
+            alpha == kCGImageAlphaPremultipliedLast);
+}
+
+// Returns a copy of the given image, adding an alpha channel if it doesn't already have one
+- (UIImage *)imageWithAlpha:(CGFloat)alpha
+{
+    if ([self hasAlpha]) {
+        return self;
+    }
+    
+    CGImageRef imageRef = self.CGImage;
+    size_t width = CGImageGetWidth(imageRef);
+    size_t height = CGImageGetHeight(imageRef);
+    
+    // The bitsPerComponent and bitmapInfo values are hard-coded to prevent an "unsupported parameter combination" error
+    CGContextRef offscreenContext = CGBitmapContextCreate(NULL,
+                                                          width,
+                                                          height,
+                                                          8,
+                                                          0,
+                                                          CGImageGetColorSpace(imageRef),
+                                                          kCGBitmapByteOrderDefault | kCGImageAlphaPremultipliedFirst);
+    
+    // Draw the image into the context and retrieve the new image, which will now have an alpha layer
+    CGContextSetAlpha(offscreenContext, alpha);
+    CGContextDrawImage(offscreenContext, CGRectMake(0, 0, width, height), imageRef);
+    CGImageRef imageRefWithAlpha = CGBitmapContextCreateImage(offscreenContext);
+    UIImage *imageWithAlpha = [UIImage imageWithCGImage:imageRefWithAlpha];
+    
+    // Clean up
+    CGContextRelease(offscreenContext);
+    CGImageRelease(imageRefWithAlpha);
+    
+    return imageWithAlpha;
+}
+
+@end
+
 @implementation UIImage (DZImagePool)
 
 + (UIImage *)templateImageWithName:(NSString *)name
@@ -40,21 +92,13 @@ static NSMutableDictionary * _imagePool;
     UIImage * image = [_imagePool objectForKey:transparentName];
     if (image == nil) {
         UIImage * oldImage = [UIImage imageNamed:name];
-        CGImageRef cgImage = [oldImage CGImage];
-        size_t width = CGImageGetWidth(cgImage);
-        size_t height = CGImageGetHeight(cgImage);
-        CGContextRef context = CGBitmapContextCreate(NULL, width, height,
-                                                     CGImageGetBitsPerComponent(cgImage),
-                                                     0,
-                                                     CGImageGetColorSpace(cgImage),
-                                                     CGImageGetBitmapInfo(cgImage));
-        CGContextSetAlpha(context, 0.5);
-        CGContextDrawImage(context, CGRectMake(0, 0, width, height), cgImage);
-        CGImageRef cgTransImage = CGBitmapContextCreateImage(context);
-        image = [[UIImage imageWithCGImage:cgTransImage]imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-        [_imagePool setObject:image forKey:transparentName];
-        CGContextRelease(context);
-        CGImageRelease(cgTransImage);
+        image = [[oldImage imageWithAlpha:0.5]imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        if (image != nil) {
+            [_imagePool setObject:image forKey:transparentName];
+        } else {
+            image = [UIImage templateImageWithName:name];
+            NSLog(@"[ERROR] cannot create transparent image.");
+        }
     }
     return image;
 }
